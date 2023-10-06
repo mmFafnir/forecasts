af4ad75aa897dc4e8df3417198aae79fd3275793
om-sv <om-sv2@mail.ru>
2023-10-06 13:47:09 +0300

    add slider

--- src/components/EventSlider/index.tsx
+++ src/components/EventSlider/index.tsx
@@ -5,6 +5,10 @@ import Image from 'next/image';
 import TimeIcon from '../UI/Icons/TimeIcon';
 import { Animate } from '@/assets/scripts/animateSlider';
 
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+
 const array = [1,2,3,4,5,6,7,8,9,10 ]
 
 const EventSlider:FC = () => {
@@ -12,48 +16,63 @@ const EventSlider:FC = () => {
 
     useEffect(() => {
         if(!sliderRef.current) return
-        new Animate(sliderRef.current)
+        // new Animate(sliderRef.current)
     }, [])
 
     return (
         <div className={styles.body}>
             <h4>Похожие события:</h4>
             <div ref={sliderRef} className={styles.slider}>
-                {
-                    array.map((slider, index) => (
-                        <div key={index} className={styles.slide}>
-                            <div className={styles.team}>
-                                <Image 
-                                    src={'/img/teams/1.png'}
-                                    alt='Манчестер Юнайтед (Manchester United)'
-                                    width={104}
-                                    height={75}
-                                />
-                                <p>Манчестер Юнайтед (Manchester United)</p>
-                            </div>
-                            <div className={styles.date}>    
-                                <p>20 сентября 2023</p>
-                                <p>
-                                    <span>19:45</span>
-                                    <span>UTC+3</span>
-                                </p>
-                                <p className={styles.time}>
-                                    <TimeIcon />
-                                    <span>Осталось: 5 часов</span>
-                                </p>
-                            </div>
-                            <div className={styles.team}>
-                                <Image 
-                                    src={'/img/teams/2.png'}
-                                    alt='Копенгаген (København)'
-                                    width={104}
-                                    height={75}
-                                />
-                                <p>Манчестер Юнайтед (Manchester United)</p>
-                            </div>
-                        </div>
-                    ))
-                }
+                <Swiper 
+                    navigation
+                    pagination={{ clickable: true }}
+                    spaceBetween={20}
+                    slidesPerView={'auto'}
+                    className='event-slider'
+                    direction='horizontal'
+                    autoplay={{
+                        delay: 0,
+                    }}
+                    loop={true}
+                >
+                    {
+                        array.map((slider, index) => (
+                            <SwiperSlide key={index}>
+                                <div  className={styles.slide}>
+                                    <div className={styles.team}>
+                                        <Image 
+                                            src={'/img/teams/1.png'}
+                                            alt='Манчестер Юнайтед (Manchester United)'
+                                            width={104}
+                                            height={75}
+                                        />
+                                        <p>Манчестер Юнайтед (Manchester United)</p>
+                                    </div>
+                                    <div className={styles.date}>    
+                                        <p>20 сентября 2023</p>
+                                        <p>
+                                            <span>19:45</span>
+                                            <span>UTC+3</span>
+                                        </p>
+                                        <p className={styles.time}>
+                                            <TimeIcon />
+                                            <span>Осталось: 5 часов</span>
+                                        </p>
+                                    </div>
+                                    <div className={styles.team}>
+                                        <Image 
+                                            src={'/img/teams/2.png'}
+                                            alt='Копенгаген (København)'
+                                            width={104}
+                                            height={75}
+                                        />
+                                        <p>Манчестер Юнайтед (Manchester United)</p>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </div>
     );
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { FC, ReactNode, useEffect, useState } from 'react'
+import { FC, ReactNode, useEffect, useState } from 'react'
 import styles from './sidebar.module.scss';
 import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
